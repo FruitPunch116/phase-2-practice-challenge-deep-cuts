b26ab36de71843ffd4e4886855ca64c151319146
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-07-13 20:06:06 -0400

    create fetch and states

--- src/components/TracksPage.jsx
+++ src/components/TracksPage.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './Search'
 import AddTrackForm from './AddTrackForm'
 import TracksList from './TracksList'
 
 function TracksPage() {
+    //useEffect fetch to back end
+    //set estate with recived data
+    //pass data down as prop
 
-    
+  const [tracklist, setTrackList] = useState([])
+  const [searchInput, setSearchInput] = useState("")
+
+  useEffect (()=>{
+    fetch("http://localhost:8001/tracks")
+    .then(r => r.json())
+    .then(data => setTrackList(data))
+  }, [])
+
   return (
     <div>
-      <Search />
-      <AddTrackForm />
-      <TracksList />
+      <Search searchInput={searchInput} setSearchInput={setSearchInput}/>
+      <AddTrackForm tracklist={tracklist} setTrackList={setTrackList}/>
+      <TracksList tracklist={tracklist} searchInput={searchInput}/>
     </div>
   )
 }
